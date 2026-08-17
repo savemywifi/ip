f274b12a09d7b4c65abdc8460a4010117885da20
Jian Yi <jianyilee27@gmail.com>
2026-08-17 17:18:36 +0800

    Add Greet and Exit
    The chatbot currently does not greet the user and say goodbye on exit.
    
    This makes the chatbot feel less friendly.
    
    A greeting and exit message have been added with templates.
    
    The templates make it easier for changes to be made to these messages if needed.

--- src/main/java/Bern.java
+++ src/main/java/Bern.java
@@ -1,11 +1,28 @@
 public class Bern {
+
+    private static final String messageLine = "____________________________________\n";
+    public static final String chatbotBanner = " ____                  \n"
+            + "|  _ \\                 \n"
+            + "| |_) | ___ _ __ _ __  \n"
+            + "|  _ < / _ \\ '__| '_ \\ \n"
+            + "| |_) |  __/ |  | | | |\n"
+            + "|____/ \\___|_|  |_| |_|";
+    private static final String chatbotName = "Bern Tokens";
+    private static final String greetingTemplate = "%s\n"
+            + messageLine
+            + "Hello! I'm %s.\n"
+            + "What can I do for you?\n";
+
+    private static String getGreetingMessage() {
+        return messageLine + String.format(greetingTemplate, chatbotBanner ,chatbotName) + messageLine;
+    }
+
+    private static String getExitMessage() {
+        return "Bye. Hope to see you again soon!\n" + messageLine;
+    }
+
     public static void main(String[] args) {
-        String banner = " ____                  \n"
-                + "|  _ \\                 \n"
-                + "| |_) | ___ _ __ _ __  \n"
-                + "|  _ < / _ \\ '__| '_ \\ \n"
-                + "| |_) |  __/ |  | | | |\n"
-                + "|____/ \\___|_|  |_| |_|";
-        System.out.println(banner);
+        System.out.print(getGreetingMessage());
+        System.out.print(getExitMessage());
     }
 }
