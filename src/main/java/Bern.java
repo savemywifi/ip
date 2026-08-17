4d18bde5029a2e9bee603fb864a9f4ea5ce9fc66
Jian Yi <jianyilee27@gmail.com>
2026-08-17 17:45:12 +0800

    Add echo functionality to chatbot
    Chatbot does not do anything.
    
    Chatbot should do something.
    
    Added ability for chatbot to repeatedly echo commands from user input.
    Chatbot exits when user inputs "bye" (case insensitive).
    
    This gives the chatbot something to do instead of quitting out
    immediately.
    
    Echoed input is stripped before being printed to standard output.

--- src/main/java/Bern.java
+++ src/main/java/Bern.java
@@ -1,27 +1,76 @@
+import java.util.Scanner;
+
 public class Bern {
-    private static final String messageLine = "____________________________________\n";
-    private static final String chatbotBanner = " ____                  \n"
+    private static final String MESSAGE_LINE = "____________________________________\n";
+    private static final String CHATBOT_BANNER = " ____                  \n"
             + "|  _ \\                 \n"
             + "| |_) | ___ _ __ _ __  \n"
             + "|  _ < / _ \\ '__| '_ \\ \n"
             + "| |_) |  __/ |  | | | |\n"
             + "|____/ \\___|_|  |_| |_|";
-    private static final String chatbotName = "Bern Tokens";
-    private static final String greetingTemplate = "%s\n"
-            + messageLine
-            + "Hello! I'm %s.\n"
-            + "What can I do for you?\n";
-
+    private static final String CHATBOT_NAME = "Bern Tokens";
+    private static final String GREETING_TEMPLATE = "%s\n"
+            + MESSAGE_LINE
+            + "> Hello! I'm %s.\n"
+            + "> What can I do for you?\n";
+    private static final String KEYWORD_EXIT = "bye";
+    /**
+     * Returns greeting message as a String.
+     *
+     * @return Greeting message to the user.
+     */
     private static String getGreetingMessage() {
-        return messageLine + String.format(greetingTemplate, chatbotBanner ,chatbotName) + messageLine;
+        return MESSAGE_LINE + String.format(GREETING_TEMPLATE, CHATBOT_BANNER, CHATBOT_NAME) + MESSAGE_LINE;
     }
 
+    /**
+     * Returns exit message as a String.
+     *
+     * @return Exit message to the user.
+     */
     private static String getExitMessage() {
-        return "Bye. Hope to see you again soon!\n" + messageLine;
+        return "> Bye. Hope to see you again soon!\n" + MESSAGE_LINE + MESSAGE_LINE;
     }
+
+    /**
+     * Prompts the user for an input, then closes the input with a message line
+     *
+     * @param sc Scanner to receive input from
+     *
+     * @return Received input, stripped of whitespace
+     */
+    private static String promptForInput(Scanner sc) {
+        String input = sc.nextLine();
+        System.out.print(MESSAGE_LINE);
+        return input.strip();
+    }
+
+    /**
+     * Echoes the given message to the standard output
+     *
+     * @param msg The message to echo
+     */
+    private static void echoMessage(String msg) {
+        System.out.print("> " + msg + "\n" + MESSAGE_LINE);
+    }
+
 
     public static void main(String[] args) {
         System.out.print(getGreetingMessage());
+
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            String input = promptForInput(sc);
+
+            if (input.equalsIgnoreCase(KEYWORD_EXIT)) {
+                break;
+            }
+
+            echoMessage(input);
+        }
+
+        sc.close();
         System.out.print(getExitMessage());
     }
 }
