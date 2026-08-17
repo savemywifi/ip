8ecbfd9792831acb832ac074a2416bce2bf51758
Jian Yi <jianyilee27@gmail.com>
2026-08-17 17:12:04 +0800

    Rename Duke to Bern
    Chatbot is to be differentiated from placeholder name Duke. Renamed all
    instances of Duke to Bern and updated ASCII Banner art to reflect name
    changes.
    
    Chatbot name is to be differentiable so separate submissions of the iP
    by different students can be more easily identified by the teaching
    team.
    
    Let's rename every instance of Duke to Bern and update the ASCII Banner
    art to reflect this change.
    
    Bern, a.k.a. Bern Tokens, is a reference to AI agents burning tokens.

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Bern User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Bern.java
@@ -0,0 +1,11 @@
+public class Bern {
+    public static void main(String[] args) {
+        String banner = " ____                  \n"
+                + "|  _ \\                 \n"
+                + "| |_) | ___ _ __ _ __  \n"
+                + "|  _ < / _ \\ '__| '_ \\ \n"
+                + "| |_) |  __/ |  | | | |\n"
+                + "|____/ \\___|_|  |_| |_|";
+        System.out.println(banner);
+    }
+}
