b2cd6e65628488f3e2df48b9fd13eefd98686138
Jian Yi <jianyilee27@gmail.com>
2026-08-28 00:00:32 +0800

    Add JUnit Tests for DateTime and ParseFactory
    There are no tests written at the moment.
    
    This means when software changes, old code may break without convenient
    ways to verify which functionality has failed.
    
    Let's:
    * Add a DateTimeTest class which tests if DateTime performs correctly
      with nullary Time fields
    * Add a ParseFactoryTest which tests if ParseFactory performs correctly
      with empty keyword arrays and throws exceptions correctly when there
      are no arguments supplied after each keyword
    
    This helps to ensure that critical functionality can be easily tested
    and verify that these functions are working even after changes in code.

--- src/main/java/bern/datetime/DateTime.java
+++ src/main/java/bern/datetime/DateTime.java
@@ -6,14 +6,14 @@ import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 
 public class DateTime implements Comparable<DateTime> {
-    private static final DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd MMMM yyyy");
-    private static final DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("hh.mma");
+    static final DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd MMMM yyyy");
+    static final DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("hh.mma");
     static final String separator = " @ ";
 
     private final LocalDate localDate;
     private final LocalTime localTime;
 
-    public DateTime(LocalDate localDate, LocalTime localTime) {
+    DateTime(LocalDate localDate, LocalTime localTime) {
         this.localDate = localDate;
         this.localTime = localTime;
     }
@@ -27,11 +27,28 @@ public class DateTime implements Comparable<DateTime> {
         return String.format("%s%s%s", localDate.format(dateFormatter), separator, localTime.format(timeFormatter));
     }
 
+    /**
+     * Compares two DateTimes. Returns a negative integer if the other date/time is after the current date/time, 0 if
+     * they are equal, or a positive integer if the other date/time. is before the current date/time. If one object has
+     * no specified time value (it is null), that time will be "before" the DateTime with a specified time field if
+     * they contain the same date.
+     *
+     * @param other the object to be compared.
+     * @return An integer indicating which DateTime object is greater.
+     */
     @Override
     public int compareTo(DateTime other) {
         int dayDifference = (int) ChronoUnit.DAYS.between(other.localDate, this.localDate);
         if (dayDifference != 0) {
             return dayDifference;
+        }
+
+        if (this.localTime == null && other.localTime == null) {
+            return 0;
+        } else if (this.localTime == null) {
+            return -1;
+        } else if (other.localTime == null) {
+            return 1;
         }
 
         return (int) ChronoUnit.MINUTES.between(other.localTime, this.localTime);
