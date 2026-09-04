b8b7e27a6123b8f26f56b758cec19f5a93c66117
Jian Yi <jianyilee27@gmail.com>
2026-09-04 13:33:44 +0800

    Update profile icons for Bern and User
    Profile icons for Bern and User are the defaults from SE-Edu
    
    These are not representative of the actual project
    
    Let's:
    * Change Bern's profile icon to be a fire
    * Change User's profile icon to be a log
    
    This fits better with the customised name Bern Tokens.

--- src/main/java/bern/ui/MainWindow.java
+++ src/main/java/bern/ui/MainWindow.java
@@ -26,8 +26,8 @@ public class MainWindow extends AnchorPane {
 
     private Controller control;
 
-    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
-    private Image bernImage = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
+    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/DaLog.png"));
+    private Image bernImage = new Image(this.getClass().getResourceAsStream("/images/DaBern.png"));
 
     /**
      * Initialises the relevant properties on startup
